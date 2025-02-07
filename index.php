02368d857aa07570b7df9eeda23ee39a8b036bb0
ansari2025 <saddam.ansari@oaksol.in>
2025-02-07 22:43:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
 // Include the file
-require 'token.php';
+require 'Token.php';
 
 // Define a key
 const KEY = 'testkey';
 
 // Generate token
-$token = token::Sign(['id' => 'std_id'],['stu_type' => 'stu_type'], KEY, 60*5);
+$token = Token::Sign(['stu_type' => 'type'], KEY, 60*5);
 
 
 // Vefity token
-$payload = token::Verify($token, KEY);
+$payload = Token::Verify($token, KEY);
